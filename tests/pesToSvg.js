0327738be090bd7c5d1109dbd6ecd8171c985c39
GuillaumeMMM <guillaume.meigniez@gmail.com>
2025-05-29 20:25:36 +0200

    Add test and update build script.

--- tests/pesToSvg.js
+++ tests/pesToSvg.js
@@ -1,17 +1,17 @@
-import { EmbPattern } from "../dist/EmbPattern.js";
-import { read } from "../dist/PesReader.js";
-import { writeSvg } from "../dist/writeSvg.js";
-import { PyFile } from "../dist/PyFile.js";
+import { EmbPattern } from "../dist/types/EmbPattern.js";
+import { read } from "../dist/pesReader.js";
+import { write } from "../dist/svgWriter.js";
+import { PyFile } from "../dist/types/PyFile.js";
 import { readFileSync, writeFileSync } from "fs";
 
 function loadPesFromArray(array) {
   const pesPyFile = new PyFile(array);
   let pattern = new EmbPattern();
   read(pesPyFile, pattern);
-  const data = writeSvg(pattern);
+  const data = write(pattern);
   writeFileSync("./tests/out.svg", data);
 }
 
 // Read the file synchronously using fs
-const buffer = readFileSync("./flowers.pes");
+const buffer = readFileSync("tests/flowers.pes");
 loadPesFromArray(buffer);
